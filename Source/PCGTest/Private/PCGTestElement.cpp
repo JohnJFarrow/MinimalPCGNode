980057c87c0818a984cd0bbdb2ff1f4e8f7cac1b
JohnJFarrow <105021048+JohnJFarrow@users.noreply.github.com>
2026-03-07 16:35:48 +1300

    initial

---
+++ Source/PCGTest/Private/PCGTestElement.cpp
@@ -0,0 +1,162 @@
+
+#include "PCGTestElement.h"
+#include "PCGContext.h"
+#include "PCGPin.h"
+
+#define LOCTEXT_NAMESPACE "PCGUnrealCode"
+
+namespace
+{
+	const FName NodeName("TestNode");
+	const FText NodeTitle = FText::Format(LOCTEXT("NodeTitle", "PCG Unrealcode | {0}"), FText::FromName(NodeName));
+	const FText Tooltip = LOCTEXT("Tooltip", "Copies input to output");
+}
+
+// Begin UPCGSettings interface
+#if WITH_EDITOR
+FName UPCGTestElement::GetDefaultNodeName() const
+{
+	return NodeName;
+}
+
+FText UPCGTestElement::GetDefaultNodeTitle() const
+{
+	return NodeTitle;
+}
+
+FText UPCGTestElement::GetNodeTooltipText() const
+{
+	return Tooltip;
+}
+
+EPCGSettingsType UPCGTestElement::GetType() const
+{
+	return EPCGSettingsType::Spatial;
+}
+
+bool UPCGTestElement::HasDynamicPins() const
+{
+	return true;
+}
+
+
+#endif
+
+
+FPCGDataTypeIdentifier UPCGTestElement::GetCurrentPinTypesID(const UPCGPin* InPin) const
+{
+	// Returns the current pin types, which can either be the static types from the pin properties, or a dynamic type based on connected edges.
+	// By default we set output pin types to the union of the default input pin incident edge types, if it is dynamic and the default input exists.
+
+	// PCGex has this 
+	/*
+	if (!InPin->IsOutputPin() || InPin->Properties.Label == PCGPinConstants::DefaultInputLabel || InPin->Properties.Label == PCGExRecursionTracker::OutputContinueLabel || InPin->Properties.Label == PCGExRecursionTracker::OutputStopLabel)
+	{
+		return Super::GetCurrentPinTypesID(InPin);
+	}
+
+	FPCGDataTypeIdentifier Id = FPCGDataTypeInfoParam::AsId();
+	if (InPin->Properties.Label == PCGExRecursionTracker::OutputProgressLabel)
+	{
+		Id.CustomSubtype = static_cast<int32>(EPCGMetadataTypes::Float);
+	}
+	else if (InPin->Properties.Label == PCGExRecursionTracker::OutputIndexLabel)
+	{
+		Id.CustomSubtype = static_cast<int32>(EPCGMetadataTypes::Integer32);
+	}
+	else if (InPin->Properties.Label == PCGExRecursionTracker::OutputRemainderLabel)
+	{
+		Id.CustomSubtype = static_cast<int32>(EPCGMetadataTypes::Integer32);
+	}
+
+	return Id;
+	*/
+
+	const FName PinName = InPin->GetFName();
+
+	// In/Out = Spline Data
+	if (PinName == PCGPinConstants::DefaultInputLabel)
+	{
+		return EPCGDataType::Spatial;
+	}
+
+	return Super::GetCurrentPinTypesID(InPin);
+}
+
+
+TArray<FPCGPinProperties> UPCGTestElement::InputPinProperties() const
+{
+	// Init pins - increase the number to add more pins
+	TArray<FPCGPinProperties> PinProperties;
+
+	// Pin default ctor is no exported
+	FPCGPinProperties Pin = PinProperties.Emplace_GetRef(PCGPinConstants::DefaultInputLabel, FPCGDataTypeInfo::AsId());
+
+	Pin.Usage = EPCGPinUsage::Normal;
+	Pin.AllowedTypes = EPCGDataType::Spatial;
+	Pin.bAllowMultipleData = true;
+
+	PinProperties.Emplace(Pin);
+	return PinProperties;
+}
+
+
+TArray<FPCGPinProperties> UPCGTestElement::OutputPinProperties() const
+{
+	// See InputPinProperties as it's essentially the same, but for output pins
+
+	TArray<FPCGPinProperties> PinProperties;
+
+	FPCGPinProperties& Pin = PinProperties.Emplace_GetRef(PCGPinConstants::DefaultOutputLabel, FPCGDataTypeInfo::AsId());
+
+	Pin.Usage = EPCGPinUsage::Normal;
+	Pin.AllowedTypes = EPCGDataType::Spatial;
+	Pin.bAllowMultipleData = true;
+
+	return PinProperties;
+}
+
+
+FPCGElementPtr UPCGTestElement::CreateElement() const
+{
+	// Return your class which is the actual node behaviour
+	// Use MakeShared<T> for this
+	return MakeShared<FPCGTestElement>();
+}
+// End UPCGSettings interface
+
+// Begin IPCGElement interface
+bool FPCGTestElement::ExecuteInternal(FPCGContext* Context) const
+{
+	// Example implementation
+	// This is the behaviour of your node
+	// You can grab data from your pins and your settings directly from the Context arg
+
+	// Add some basic profiler support
+	TRACE_CPUPROFILER_EVENT_SCOPE(FPCGTestElement::Execute);
+
+	// Double check settings
+	check(Context != nullptr);
+
+	// Resolve settings
+	const UPCGTestElement* const Settings = Context->GetInputSettings<UPCGTestElement>();
+	check(Settings != nullptr);
+
+	// Get input pin with the default label
+	TArray<FPCGTaggedData> Sources = Context->InputData.GetInputsByPin(PCGPinConstants::DefaultInputLabel);
+
+	// No data, so early out
+	if (Sources.Num() == 0)
+	{
+		return true;
+	}
+
+	// Example - will just spit out the data coming in
+	// You would add your own outputs here
+	Context->OutputData.TaggedData.Append(Sources);
+
+	return true;
+}
+// End IPCGElement interface
+
+#undef LOCTEXT_NAMESPACE
---
+++ Source/PCGTest/Public/PCGTestElement.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "PCGSettings.h"
+
+#include "PCGTestElement.generated.h"
+
+UCLASS(BlueprintType, ClassGroup = (Procedural), Category = "Unrealcode | Test Nodes")
+class PCGTEST_API UPCGTestElement : public UPCGSettings
+{
+	GENERATED_BODY()
+
+public:
+	// Begin UPCGSettings interface
+public:
+	#if WITH_EDITOR
+	virtual FName GetDefaultNodeName() const override;
+	virtual FText GetDefaultNodeTitle() const override;
+	virtual FText GetNodeTooltipText() const override;
+	virtual EPCGSettingsType GetType() const override;
+	#endif
+	virtual bool HasDynamicPins() const override;
+	virtual FPCGDataTypeIdentifier GetCurrentPinTypesID(const UPCGPin* InPin) const override;
+protected:
+	virtual TArray<FPCGPinProperties> InputPinProperties() const override;
+	virtual TArray<FPCGPinProperties> OutputPinProperties() const override;
+	virtual FPCGElementPtr CreateElement() const override;
+	// End UPCGSettings interface
+
+public:
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Settings", meta = (PCG_Overridable))
+	bool bExampleSetting = false;
+};
+
+class PCGTEST_API FPCGTestElement : public IPCGElement
+{
+protected:
+	// Begin IPCGElement interface
+	virtual bool ExecuteInternal(FPCGContext* Context) const override;
+	// End IPCGElement interface
+};
